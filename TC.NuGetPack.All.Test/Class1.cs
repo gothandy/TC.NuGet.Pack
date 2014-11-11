77a852ad8ede56433dd12a8d062510cfb0c85f9a
gothandy <gothandy@hotmail.com>
2014-11-11 15:53:20 +0000

    Initial NuGet packages.

---
+++ TC.NuGetPack.All.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC.NuGetPack.All.Test
+{
+    public class Class1
+    {
+    }
+}
